15b2e2018e6cfda3828a36dc734dfde0050ac3dd
natalya.dolmatova <dolmatova-nata@mail.ru>
2023-07-25 08:59:14 +0300

    refactoring

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class BaseTest {
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+
+public class BaseTest {
+    @BeforeAll
+    static void beforeAll() {
+
+        Configuration.baseUrl = "https://career.level.travel/middle_qa";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.browserSize = "1920x1080";
+    }
 }
--- src/test/java/VacancyTest.java
+++ src/test/java/VacancyTest.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class VacancyTest {
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Configuration.baseUrl;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class VacancyTest extends BaseTest {
+
+    @Test
+    void logoMustBeOnThePage() {
+        open(baseUrl);
+        $(".t339__blocklogo").shouldBe(visible);
+    }
+
+    @Test
+    void nameOfVacancyMustBeQa() {
+        open(baseUrl);
+        $(".t338__title").shouldHave(text("Middle QA engineer"));
+    }
+    @Test
+    void describeVacancyShouldBeOnThePage() {
+        open(baseUrl);
+        $(".t004").shouldBe(visible);
+    }
+
+    @Test
+    void describeCompanyShouldBeOnThePage() {
+        open(baseUrl);
+        $(".t487__descr").shouldBe(visible);
+    }
+    @Test
+    void mailShouldBeOnThePage() {
+        open(baseUrl);
+        $(".t551__contacts").shouldHave(text("joinus@level.travel"));
+    }
 }
