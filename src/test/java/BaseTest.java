3bdd01be9397fdf022a9e90597e1a2612214c2a7
natalya.dolmatova <dolmatova-nata@mail.ru>
2023-07-28 20:19:38 +0300

    refactoring

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,12 +1,48 @@
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.WebDriverRunner;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import config.WebConfig;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
 
 public class BaseTest {
-    @BeforeAll
-    static void beforeAll() {
+    static WebConfig config = ConfigFactory.create(WebConfig.class, System.getProperties());
 
-        Configuration.baseUrl = "https://career.level.travel/middle_qa";
-        Configuration.pageLoadStrategy = "eager";
-        Configuration.browserSize = "1920x1080";
+    @BeforeAll
+    static void init() {
+        Configuration.pageLoadStrategy = config.getPageLoadStrategy();
+        Configuration.browser = config.getBrowser();
+        Configuration.browserVersion = config.getBrowserVersion();
+        Configuration.browserSize = config.getBrowserSize();
+        Configuration.baseUrl = config.getBaseUrl();
+        Configuration.remote = config.getRemoteUrl();
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
+    }
+
+    @BeforeEach
+    void addListener() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.attachScreenshot("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLog();
+        Attach.addVideo();
+        WebDriverRunner.driver().close();
     }
 }
--- src/test/java/helpers/Attach.java
+++ src/test/java/helpers/Attach.java
@@ -14,41 +14,33 @@ import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 import static org.openqa.selenium.logging.LogType.BROWSER;
 
 public class Attach {
-    @Attachment(value = "{attachName}", type = "image/png")
-    public static byte[] screenshotAs(String attachName) {
+    @Attachment(value = "{attachName}", type = "image/png", fileExtension = "png")
+    public static byte[] attachScreenshot(String attachName) {
         return ((TakesScreenshot) getWebDriver()).getScreenshotAs(OutputType.BYTES);
     }
-
-    @Attachment(value = "Page source", type = "text/plain")
+    @Attachment(value = "page source", type = "text/plain")
     public static byte[] pageSource() {
         return getWebDriver().getPageSource().getBytes(StandardCharsets.UTF_8);
     }
-
     @Attachment(value = "{attachName}", type = "text/plain")
     public static String attachAsText(String attachName, String message) {
         return message;
     }
 
-    public static void browserConsoleLogs() {
-        attachAsText(
-                "Browser console logs",
-                String.join("\n", Selenide.getWebDriverLogs(BROWSER))
-        );
+    public static void browserConsoleLog(){
+        attachAsText("Browser console log",String.join("\n", Selenide.getWebDriverLogs(BROWSER)));
     }
-
     @Attachment(value = "Video", type = "text/html", fileExtension = ".html")
     public static String addVideo() {
         return "<html><body><video width='100%' height='100%' controls autoplay><source src='"
                 + getVideoUrl()
                 + "' type='video/mp4'></video></body></html>";
     }
-
-    public static URL getVideoUrl() {
+    public static URL getVideoUrl(){
         String videoUrl = "https://selenoid.autotests.cloud/video/" + sessionId() + ".mp4";
-//        System.out.println(sessionId());
-        try {
+        try{
             return new URL(videoUrl);
-        } catch (MalformedURLException e) {
+        } catch (MalformedURLException e){
             e.printStackTrace();
         }
         return null;
